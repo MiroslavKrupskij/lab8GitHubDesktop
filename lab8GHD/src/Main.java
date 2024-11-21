f770e0594bcf542ee00545697d4956460a4dff66
MiroslavKrupskij <miroslavkrupskij@gmail.com>
2024-11-21 19:30:40 +0200

    second commit

--- lab8GHD/src/Main.java
+++ lab8GHD/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("Sum: " + calcSum(282, 474));
+    }
+    public static int calcSum(int a, int b){
+        return a + b;
     }
 }
